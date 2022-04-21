2ab7f6efe7e69d085e83a2f0a18608a6431a77c2
fabricioalvs <31606241+fabricioalvs@users.noreply.github.com>
2022-04-20 21:38:19 -0300

    Update rasp_receive_data_arduino.py

--- código-fonte/rasp_receive_data_arduino.py
+++ código-fonte/rasp_receive_data_arduino.py
@@ -4,9 +4,7 @@ import string
 import time
 import requests
 import math
-import random
-import threading
-
+import sys
 TOKEN = "BBFF-L48Awxo7p33L5GSmURXCcaqt3Cl5Bl"  # Put your TOKEN here
 DEVICE_LABEL = "ECG_signal_monitoring_raspberry"  # Put your device label here
 VARIABLE_LABEL_1 = "ecg_sensor_data"  # Put your first variable label here
@@ -15,15 +13,18 @@ VARIABLE_LABEL_1 = "ecg_sensor_data"  # Put your first variable label here
 #global payload
 def receive_data(variable_1):
     #abertura da porta serial
-    ser = serial.Serial('/dev/ttyS0', 115200, timeout=0)
-    ser.reset_input_buffer()
-    while True:
-            serialdata=ser.read()
-            serialdata= int.from_bytes(serialdata,"little",signed=True)*(-1)
+    ser = serial.Serial('/dev/ttyS0', 115200, timeout=0.1)
+    ser.flush
+    if ser.isOpen():
+        print("connected!")
+        while True:
+            serialdata=ser.readline().decode('utf-8').rstrip()
+            if serialdata == '!':
+                serialdata=0
+            serialdata=int(serialdata)
+            #serialdata=int.from_bytes(serialdata,"little",signed=True)
             payload= {variable_1: serialdata}
             post_request(payload)
-            #lendo dados da porta serial e printando na tela
-    #return payload
 
 def post_request(payload):
     # Creates the headers for the HTTP requests
@@ -38,7 +39,7 @@ def post_request(payload):
         req = requests.post(url=url, headers=headers, json=payload)
         status = req.status_code
         attempts += 1
-        #time.sleep(0.001)
+        #time.sleep(0.1)
 
     # Processes results
     print(req.status_code, req.json())
@@ -49,14 +50,8 @@ def post_request(payload):
 
     print("[INFO] request made properly, your device is updated")
     return True
-#def main():
-    #payload = receive_data(VARIABLE_LABEL_1)
-    #post_request(payload)
 
 if __name__ == '__main__':
-    #while (True):
-    #a = threading.Thread(target=post_request,name='Thread-a',daemon=True)
+
     payload = receive_data(VARIABLE_LABEL_1)
-    #b = threading.Thread(target=main,name='Thread-b')
-    #a.start()
-    #b.start()
+
